dcfe8153b82a5e2731242f2e8f01f5aab05e470c
Danzep0922 <al221910358@gmail.com>
2020-10-05 09:01:48 -0500

    proyectoAWOS

--- resources/views/terminar_sesion.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-// Destruir todas las variables de sesión.
-$_SESSION = [];
-// Finalmente, destruir la sesión.
-session_destroy();
-header('location:index.php?info=Sesión terminada');
-?>
